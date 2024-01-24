6ab2c7a6d544041494e96b372ca1f7fb3ad05f5b
kaitlindonlon <kaitlindonlon@kaitlins-mbp.wusm-wifi.wucon.wustl.edu>
2024-01-24 13:20:43 -0600

    hello

--- src/studio1/Hello.java
+++ src/studio1/Hello.java
@@ -2,15 +2,19 @@ package studio1;
 
 import java.util.Scanner;
 
+
 public class Hello {
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
+		System.out.println("What is your Name?");
+		String name =in.nextLine();
 		System.out.println("How are you feeling today?");
 		String mood = in.nextLine();
 		
-		System.out.println("Hello and welcome to CSE 131!");
+		
+		System.out.println(name+", Hello and welcome to CSE 131!");
 		System.out.println("You seem to be feeling " + mood);
 	}
 
